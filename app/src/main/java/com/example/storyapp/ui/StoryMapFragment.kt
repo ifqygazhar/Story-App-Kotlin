daedbf2384f6bd8215fbb2d558f4741dd192f149
ifqygazhar <ifqygazhar@gmail.com>
2024-10-23 07:04:57 +0700

    feat(story): add map feature
    Add location permission and map view to view stories in location
    
    BREAKING CHANGE: Requires location permission.

--- app/src/main/java/com/example/storyapp/ui/StoryMapFragment.kt
+++ app/src/main/java/com/example/storyapp/ui/StoryMapFragment.kt
@@ -1,34 +1,46 @@
 package com.example.storyapp.ui
 
 import android.content.Intent
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.storyapp.R
 import com.example.storyapp.data.Result
 import com.example.storyapp.data.pref.UserPreferences
+import com.example.storyapp.data.remote.response.ListStoryItem
 import com.example.storyapp.databinding.FragmentStoryMapBinding
-import com.example.storyapp.ui.adapter.StoryAdapter
 import com.example.storyapp.ui.viewmodel.StoryMapViewModel
 import com.example.storyapp.ui.viewmodel.factory.StoryMapViewModelFactory
 import com.example.storyapp.util.showToast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
 
-class StoryMapFragment : Fragment() {
+class StoryMapFragment : Fragment(), OnMapReadyCallback {
 
     private val storyMapViewModel: StoryMapViewModel by viewModels {
         StoryMapViewModelFactory.getInstance(requireActivity())
     }
-    private lateinit var storyAdapter: StoryAdapter
+    private lateinit var mMap: GoogleMap
+
     private var _binding: FragmentStoryMapBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?,
+    ): View {
         _binding = FragmentStoryMapBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -36,26 +48,25 @@ class StoryMapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupRecyclerView()
-        observeViewModel()
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        setupObservers()
 
         binding.fab.setOnClickListener {
             val intent = Intent(requireContext(), AddStoryActivity::class.java)
             startActivity(intent)
         }
 
-
         val token = UserPreferences(requireContext()).getToken()
-        storyMapViewModel.getAllStoriesWithMap(token!!)
+        token?.let {
+            storyMapViewModel.getAllStoriesWithMap(it)
+        } ?: run {
+            showToast(requireContext(), "Token tidak tersedia")
+        }
     }
 
-    private fun setupRecyclerView() {
-        binding.rvStory.layoutManager = LinearLayoutManager(requireContext())
-        storyAdapter = StoryAdapter(requireContext(), emptyList())
-        binding.rvStory.adapter = storyAdapter
-    }
-
-    private fun observeViewModel() {
+    private fun setupObservers() {
         storyMapViewModel.stories.observe(viewLifecycleOwner) { result ->
             when (result) {
                 is Result.Loading -> {
@@ -66,13 +77,9 @@ class StoryMapFragment : Fragment() {
                     binding.progressBar.visibility = View.GONE
                     val stories = result.data
                     if (stories.isNotEmpty()) {
-                        storyAdapter = StoryAdapter(requireContext(), stories)
-                        binding.rvStory.adapter = storyAdapter
-                        binding.rvStory.visibility = View.VISIBLE
-                        binding.imgEmpty.visibility = View.GONE
+                        addManyMarker(stories)
                     } else {
-                        binding.rvStory.visibility = View.GONE
-                        binding.imgEmpty.visibility = View.VISIBLE
+                        Toast.makeText(context, "Tidak ada data cerita", Toast.LENGTH_SHORT).show()
                     }
                 }
 
@@ -83,7 +90,6 @@ class StoryMapFragment : Fragment() {
             }
         }
 
-
         storyMapViewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
             if (isLoading) {
                 binding.progressBar.visibility = View.VISIBLE
@@ -93,10 +99,60 @@ class StoryMapFragment : Fragment() {
         }
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        // Opsional: Atur pengaturan peta seperti jenis peta, zoom level awal, dll.
+        mMap.uiSettings.isZoomControlsEnabled = true
+    }
+
+    private fun addManyMarker(stories: List<ListStoryItem>) {
+        if (::mMap.isInitialized) {
+            val boundsBuilder = LatLngBounds.Builder()
+
+            stories.forEach { story ->
+                val lat = story.lat
+                val lon = story.lon
+                if (lat != null && lon != null) {
+                    val latLng = LatLng(lat as Double, lon as Double)
+                    mMap.addMarker(
+                        MarkerOptions()
+                            .position(latLng)
+                            .title(story.name)
+                            .snippet(story.description)
+                    )
+                    boundsBuilder.include(latLng)
+                }
+            }
+
+
+            val bounds = boundsBuilder.build()
+            setMapStyle()
+            val padding = 100
+            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, padding))
+        } else {
+            Toast.makeText(context, "Map belum siap", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setMapStyle() {
+        try {
+            val success =
+                mMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                        requireContext(),
+                        R.raw.map_style
+                    )
+                )
+            if (!success) {
+                Log.e("StoryMapFragment", "Style parsing failed.")
+            }
+        } catch (exception: Resources.NotFoundException) {
+            Log.e("StoryMapFragment", "Can't find style. Error: ", exception)
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
